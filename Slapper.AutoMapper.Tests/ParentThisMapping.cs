79c39887c0f2d89849e138481ab4daaa676e0d53
e.vdspoel <e.vdspoel@bluefieldpartners.nl>
2016-02-06 11:39:49 +0100

    Unittest

---
+++ Slapper.AutoMapper.Tests/ParentThisMapping.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Slapper.Tests
+{
+    [TestFixture]
+    public class ParentThisMapping : TestBase
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public Department Department { get; set; }
+        }
+
+        public class Department
+        {
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void Can_Populate_Self_Object_Properties()
+        {
+            dynamic person = new ExpandoObject();
+            person.PERSON_NAME = "Clark";
+            person.DEPARTMENT_NAME = "IT";
+
+            var list = new List<dynamic> { person };
+
+            var pPerson = Slapper.AutoMapper.MapDynamic<Person>(list).FirstOrDefault();
+
+            Assert.NotNull(pPerson);
+            Assert.NotNull(pPerson.Name);
+            Assert.NotNull(pPerson.Department.Name);
+        }
+    }
+
+
+}
